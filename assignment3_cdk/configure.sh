f015d14bd3cc71863cd19516bb9ef79732d6d9f0
lesterjohnabad08 <lesterjohnabad@gmail.com>
2026-04-28 21:38:27 -0500

    feat: update configure script for software installation and EFS setup and Moodle

--- assignment3_cdk/configure.sh
+++ assignment3_cdk/configure.sh
@@ -1,9 +1,43 @@
-#!/bin/sh
-# Use this to install software packages
-# This script is for EC2 userdata. All commands are executed as administrators. 
-yum update -y
-amazon-linux-extras install mariadb10.5
-amazon-linux-extras install php8.2
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+
+#!/bin/bash
+set -e
+
+# Update system
+dnf update -y
+
+# Install packages
+dnf install -y httpd php php-mysqlnd php-gd php-intl \
+php-xml php-soap php-zip php-opcache git amazon-efs-utils
+
+# Enable and start Apache
+systemctl enable --now httpd
+
+# Create and mount EFS
+mkdir -p /var/moodledata
+
+mount -t efs -o tls fs-0eaa78f89260719d1:/ /var/moodledata
+echo "fs-0eaa78f89260719d1:/ /var/moodledata efs _netdev,tls 0 0" >> /etc/fstab
+
+chown -R apache:apache /var/moodledata
+chmod 770 /var/moodledata
+
+# Install Moodle
+cd /var/www
+git clone https://github.com/moodle/moodle.git
+cd moodle
+git checkout MOODLE_502_STABLE
+chown -R apache:apache /var/www/moodle
+
+# Apache VirtualHost config
+cat > /etc/httpd/conf.d/moodle.conf <<'EOF'
+<VirtualHost *:80>
+    DocumentRoot /var/www/moodle
+    <Directory /var/www/moodle>
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+
+# Restart Apache
+systemctl restart httpd
